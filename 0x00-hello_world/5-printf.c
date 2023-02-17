1ebba9c9945fb51bf1c588e7894da1a901df21c3
sam-sny <sampsonekpo6@gmail.com>
2023-02-16 20:57:26 -0800

    Print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+* main - print line 
+*                                                 * Return: Always 0 (success)                      */                                                int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
