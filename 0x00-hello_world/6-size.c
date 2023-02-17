604209b54c112afc4ac4311bd81f749d3611413c
sam-sny <sampsonekpo6@gmail.com>
2023-02-17 06:29:39 -0800

    Size of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 char c;
 int i;
 float f;
-printf("Size of char: %lu byte\(s\)\n", sizeof(c));
+printf("Size of a char: %lu byte\(s\)\n", sizeof(c));
 printf("Size of an int: %lu byte\(s\)\n", sizeof(i));
 printf("Size of a long int: %lu byte\(s\)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte\(s\)\n", sizeof(long long int));
