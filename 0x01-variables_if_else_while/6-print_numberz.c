6da747e84e3494170b8893478cb61123db905fed
sam-sny <sampsonekpo6@gmail.com>
2023-02-18 14:05:30 -0800

    Print digit with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Print base 10 digits with Putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
+
