34cb1e01766fd2c6f95aa573286bc83c9f93cd33
sam-sny <sampsonekpo6@gmail.com>
2023-02-21 14:09:44 -0800

    Check which number is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - check for the lowercase
+ *
+ * Return: 1 (success), 0 (otherwise)
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+return (0);
+}
+
