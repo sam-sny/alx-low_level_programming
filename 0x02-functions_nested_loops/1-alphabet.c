079d8af96011adb1d89acf258b7388c4a6bb4917
sam-sny <sampsonekpo6@gmail.com>
2023-02-21 13:15:45 -0800

    _putchar alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print - print alphabet in lowercase
+ *
+ * Return: 0 always
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+putchar('\n');
+}
+
