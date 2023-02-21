882f0136e9d0b4e8106f5817fb59ea701c4a9ba8
sam-sny <sampsonekpo6@gmail.com>
2023-02-21 14:48:56 -0800

    print signs

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - check for alphabetic character
+ *
+ * Return: 0 (success)
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+return (0);
+}
 
