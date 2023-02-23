f8099ca539e42514da03b04371e325b0f5fcbf8d
sam-sny <sampsonekpo6@gmail.com>
2023-02-23 05:35:13 -0800

    Print positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - test a function to print positive and negative integers
+ * Return: 0 (successs)
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
+
