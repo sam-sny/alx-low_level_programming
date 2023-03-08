c8f21235b2147b09e91a1f3492fcb1134b352fad
sam-sny <sampsonekpo6@gmail.com>
2023-03-08 10:33:48 -0800

    C recursions

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * factorial - calculates the factorial of a given number
+ * @n: number
+ * Return: -1 if the factorial is less than 0
+ */
+int factorial(int n)
+{
+if (n  < 0)
+{
+return (-1);
+}
+if (n == 1 || n == 0)
+{
+return (1);
+}
+return (n * factorial(n -1));
+}
+
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+/**
+ * _putchar - writes out a character
+ *
+ * Return: character
+ */
+int _putchar(char c)
+{
+return (write(1,&c,1));
+}
+
